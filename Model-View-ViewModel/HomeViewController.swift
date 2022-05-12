e4dec40bd5819ab2e603b9e51a5dbeac6c19e844
sergeiKalinin35 <piolersa85@mail.ru>
2022-05-12 20:16:04 +0200

    Initial Commit

---
+++ Model-View-ViewModel/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Model-View-ViewModel
+//
+//  Created by Sergey on 12.05.2022.
+//
+
+import UIKit
+
+class HomeViewController: UITableViewController {
+    
+    
+    @IBOutlet var tableViewTwo: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+}
+
