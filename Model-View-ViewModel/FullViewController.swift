608864ce3ad292b0759515f7d1fcc59a9ed63fbd
sergeiKalinin35 <piolersa85@mail.ru>
2022-05-22 22:53:41 +0200

    next 2

---
+++ Model-View-ViewModel/FullViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FullViewController.swift
+//  Model-View-ViewModel
+//
+//  Created by Sergey on 22.05.2022.
+//
+
+import UIKit
+
+class FullViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+    
+
+   
+}
